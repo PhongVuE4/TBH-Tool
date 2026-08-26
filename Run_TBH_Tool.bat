5e934add873f4beb16705a11e136fd02864c68de
PhongVuE4 <167891262+PhongVuE4@users.noreply.github.com>
2026-08-26 22:24:39 +0700

    Delete Run_TBH_Tool.bat

--- Run_TBH_Tool.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title TBH-Tool Launcher - PVandAI
-cd /d "D:\Somethings\ToolTest"
-if exist "dist\TBH-Tool\TBH-Tool.exe" (
-    "dist\TBH-Tool\TBH-Tool.exe"
-) else (
-    "D:\Somethings\ToolTest\venv\Scripts\python.exe" main.py
-)
-pause
